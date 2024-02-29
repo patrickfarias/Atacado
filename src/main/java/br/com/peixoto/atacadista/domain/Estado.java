aef160c6da15fe8ed6abefd52567b4b85df312c9
“patrickfarias” <patrickfarias@icloud.com>
2024-02-29 10:31:10 -0300

    Estado

---
+++ src/main/java/br/com/peixoto/atacadista/domain/Estado.java
@@ -0,0 +1,29 @@
+package br.com.peixoto.atacadista.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Estado {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String nome;
+
+}
