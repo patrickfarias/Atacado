c2eca6eb04bfdd58baac8cbb7e46627501e36439
“patrickfarias” <patrickfarias@icloud.com>
2024-02-29 10:27:57 -0300

    Configuracao

---
+++ src/main/java/br/com/peixoto/atacadista/configuration/springdoc/SpringDocConfig.java
@@ -0,0 +1,45 @@
+package br.com.peixoto.atacadista.configuration.springdoc;
+
+import br.com.peixoto.atacadista.exception.ErrorMessage;
+import io.swagger.v3.core.converter.ModelConverters;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.tags.Tag;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("App Atacadista")
+                        .version("v1")
+                        .description("Aplicação para controle de Vendas da maior atacadista de Uberlândia-MG")
+                ).externalDocs(new ExternalDocumentation()
+                        .description("Acesse nosso Help")
+                        .url("https://www.peixoto.com.br")
+                ).tags(Arrays.asList(
+                        new Tag().name("Categoria").description("Gerenciamento de categorias dos produtos."),
+                        new Tag().name("Produto").description("Gerenciamento do cadastro de Produtos.")
+                )).components(new Components()
+                        .schemas(gerarSchemas()));
+    }
+    private Map<String, Schema> gerarSchemas() {
+
+        final Map<String, Schema> schemaMap = new HashMap<>();
+        final Map<String, Schema> errorMessageSchema = ModelConverters.getInstance().read(ErrorMessage.class);
+
+        schemaMap.putAll(errorMessageSchema);
+
+        return schemaMap;
+    }
+}
