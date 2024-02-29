cd439d12b94c446868b2862489847f4ebefc14dd
“patrickfarias” <patrickfarias@icloud.com>
2024-02-29 10:29:17 -0300

    resources

--- src/main/resources/consultas.sql
+++ src/main/resources/consultas.sql
@@ -4,9 +4,6 @@ select * from estado
 select * from cidade
 select * from endereco
 
-
-
-
 SELECT
     cli.id AS Id,
     cli.nome AS Cliente,
@@ -22,25 +19,3 @@ FROM
    WHERE
 	cli.id = 1;
 
-
-
-
-
-
-
-
--- Vendedor Agencia
-SELECT
-    cli.id AS Id,
-    cli.nome AS Cliente,
-    cli.endereco_logradouro AS 'Rua/Avenida',
-    cli.endereco_numero AS Numero,
-    cli.endereco_cep AS CEP,
-    c.nome AS Cidade,
-    uf.nome As UF
-FROM
-    cliente cli
-    JOIN cidade c ON v.endereco_cidade_id  = c.id
-    JOIN estado uf ON c.estado_id  = uf.id
-WHERE
-	v.id = 1;
