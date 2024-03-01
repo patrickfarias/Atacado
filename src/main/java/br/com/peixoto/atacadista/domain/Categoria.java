bd439a5fccbcff46db10848ab644d32e601508fc
“patrickfarias” <patrickfarias@icloud.com>
2024-02-29 23:05:32 -0300

    Categoria

--- src/main/java/br/com/peixoto/atacadista/domain/Categoria.java
+++ src/main/java/br/com/peixoto/atacadista/domain/Categoria.java
@@ -6,12 +6,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Categoria {
