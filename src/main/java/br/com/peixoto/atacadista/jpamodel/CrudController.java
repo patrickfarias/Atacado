da39662ded449126335a3c8887076f612f96a62f
“patrickfarias” <patrickfarias@icloud.com>
2024-03-05 14:11:17 -0300

    add JpaModel para Controller

---
+++ src/main/java/br/com/peixoto/atacadista/jpamodel/CrudController.java
@@ -0,0 +1,13 @@
+package br.com.peixoto.atacadista.jpamodel;
+
+/**
+ *
+ * @param <I> Input class
+ * @param <O> Output class
+ */
+public interface CrudController<I, O> {
+    O save(I requestBody);
+    O update(Long objectId, I requestBody);
+    void delete(Long objectId);
+
+}
