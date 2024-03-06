529f4c50756cad7d0f222497750cf4ca2a50e49a
“patrickfarias” <patrickfarias@icloud.com>
2024-03-06 03:42:15 -0300

    Ajustes

--- src/main/java/br/com/peixoto/atacadista/repository/ItemPedidoRepository.java
+++ src/main/java/br/com/peixoto/atacadista/repository/ItemPedidoRepository.java
@@ -1,10 +1,13 @@
 package br.com.peixoto.atacadista.repository;
 
 import br.com.peixoto.atacadista.domain.ItemPedido;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
+
+    List<ItemPedido> findByPedidoId(Long pedidoId);
 
 }
